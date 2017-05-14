c61650568bf51637d6c1acb26e7512d70fb8b3fa
Artur Augustyniak <artur@aaugustyniak.pl>
2017-05-14 19:12:17 +0200

    cleanup

--- factory_method_enums/target/debug/factory_method.d
+++
@@ -1,1 +0,0 @@
-/media/DATA/IT_lexicon/RUST/kod/rust_design_pattern/factory_method/target/debug/factory_method: /media/DATA/IT_lexicon/RUST/kod/rust_design_pattern/factory_method/src/main.rs
--- factory_method_enums/target/debug/factory_method_enums.d
+++
@@ -1,1 +0,0 @@
-/media/DATA/IT_lexicon/RUST/kod/rust_design_pattern/factory_method_enums/target/debug/factory_method_enums: /media/DATA/IT_lexicon/RUST/kod/rust_design_pattern/factory_method_enums/src/main.rs
