c3176aee8a886fe55387fc984c86fc67e430d273
Artur Augustyniak <artur@aaugustyniak.pl>
2017-06-01 20:33:57 +0200

    working generic strategy

--- strategy/src/generic_func_strat.rs
+++ strategy/src/generic_func_strat.rs
@@ -1,24 +1,48 @@
-fn b_sort(v: &Vec<i32>) -> Vec<i32> {
-    vec![1, 2, 3]
+extern crate rand;
+
+use self::rand::Rng;
+
+
+fn forward_sort_strategy<T: Ord + Clone>(v: &Vec<T>) -> Vec<T> {
+    let mut inner_v = v.clone();
+    inner_v.sort();
+    inner_v.to_vec()
 }
 
 
-fn q_sort(v: &Vec<i32>) -> Vec<i32> {
-    vec![4, 5, 6]
+fn reverse_sort_strategy<T: Ord + Clone>(v: &Vec<T>) -> Vec<T> {
+    let mut inner_v = v.clone();
+    inner_v.sort();
+    inner_v.reverse();
+    inner_v.to_vec()
 }
 
 
-fn sort<F, T>(v: &T, f: F) -> T where F: Fn(&T) -> T
+fn sort<F, T: Ord + Clone>(v: &T, f: F) -> T where F: Fn(&T) -> T
 {
     f(v)
 }
 
 
 pub fn run() {
-    let v = vec![4, 3, 45, 654, 2, 4, 5, 2];
-    let b_sorted = sort(&v, b_sort);
-    let q_sorted = sort(&v, q_sort);
-    println!("{:?}", v);
-    println!("{:?}", b_sorted);
-    println!("{:?}", q_sorted);
+    let mut rng = rand::thread_rng();
+    for _ in 0..10 {
+        if rng.gen() {
+            let v = vec![4, 3, 45, 654, 2, 4, 5, 2];
+            println!("{:?}", v);
+            if rng.gen() {
+                println!("{:?}", sort(&v, forward_sort_strategy));
+            } else {
+                println!("{:?}", sort(&v, reverse_sort_strategy));
+            }
+        } else {
+            let v = vec!["a", "c", "b"];
+            println!("{:?}", v);
+            if rng.gen() {
+                println!("{:?}", sort(&v, forward_sort_strategy));
+            } else {
+                println!("{:?}", sort(&v, reverse_sort_strategy));
+            }
+        }
+    }
 }
--- strategy/src/main.rs
+++ strategy/src/main.rs
@@ -1,7 +1,7 @@
 pub mod generic_func_strat;
 
 use generic_func_strat::run as gfs_run;
-
+//cargo rustc -- -Z unstable-options --pretty=expanded
 fn main(){
     gfs_run();
 
