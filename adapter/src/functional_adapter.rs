c5e667be492746d545c51337342f48fb29522e99
Artur Augustyniak <artur@aaugustyniak.pl>
2017-06-22 22:13:48 +0200

    immutable fold adapter

--- adapter/src/functional_adapter.rs
+++ adapter/src/functional_adapter.rs
@@ -11,6 +11,7 @@ pub trait ChangeVelocity {
     fn accelerate(self, t: Duration) -> ImmutableLegacyTrainAdaptor;
     fn decelerate(self, t: Duration) -> ImmutableLegacyTrainAdaptor;
     fn textural_dump(self) -> ImmutableLegacyTrainAdaptor;
+    fn mov(self, directed_duration: &i32) -> ImmutableLegacyTrainAdaptor;
 }
 
 #[derive(Debug)]
@@ -37,16 +38,30 @@ fn accelerate_with<F>(lt: LegacyTrain, t: Duration, op: F) -> ImmutableLegacyTra
 impl ChangeVelocity for ImmutableLegacyTrainAdaptor {
     fn accelerate(self, t: Duration) -> ImmutableLegacyTrainAdaptor {
         let fw_acceleration = |x, y| x + y;
-        accelerate_with(self.adaptee, t, fw_acceleration).textural_dump()
+        accelerate_with(self.adaptee, t, fw_acceleration)
     }
 
     fn decelerate(self, t: Duration) -> ImmutableLegacyTrainAdaptor {
         let rev_acceleration = |x, y| x - y;
-        accelerate_with(self.adaptee, t, rev_acceleration).textural_dump()
+        accelerate_with(self.adaptee, t, rev_acceleration)
+        //.textural_dump()
     }
     fn textural_dump(self) -> ImmutableLegacyTrainAdaptor {
         println!("Impure debug {}", to_string(&self));
         self
+    }
+    fn mov(self, directed_duration: &i32) -> ImmutableLegacyTrainAdaptor {
+        match directed_duration.cmp(&0) {
+            Ordering::Less =>
+                {
+                    let v: i32 = *directed_duration;
+                    let v = v.abs();
+                    self.decelerate(Duration::new(v as u64, 0)).textural_dump()
+                }
+            _ => {
+                self.accelerate(Duration::new(*directed_duration as u64 as u64, 0)).textural_dump()
+            }
+        }
     }
 }
 
@@ -61,23 +76,7 @@ impl Clone for ImmutableLegacyTrainAdaptor {
 }
 
 
-pub fn functional_adapter_use() {
-    println!("-------------------- {} --------------------", file!());
-    let legacy_train = LegacyTrain::new(Direction::Forward, 12);
-    let train = ImmutableLegacyTrainAdaptor { adaptee: legacy_train };
-    println!("{}", to_string(&train));
-    let d1 = Duration::new(5, 0);
-    let d2 = Duration::new(12, 0);
-    //nowy bo to jest inny pociąg z dziedziny "pociągów z możliwymi prędkościami"
-    // po n przekrztałeceniach
-    let new_algebraic_train = train
-        .accelerate(d1)
-        .decelerate(d1)
-        .decelerate(d1)
-        .decelerate(d2)
-        .accelerate(d1);
-    println!("{}", to_string(&new_algebraic_train));
-
+fn mutable_collect_to_final_pos() {
     println!("-------------------- {} --------------------", "collect() to final pos");
 
     let legacy_train = LegacyTrain::new(Direction::Forward, 12);
@@ -105,4 +104,35 @@ pub fn functional_adapter_use() {
     for x in final_pos {
         x.textural_dump();
     }
+}
+
+
+fn immutable_fold_to_final_pos() {
+    println!("-------------------- {} --------------------", "fold() to final pos");
+    let legacy_train = LegacyTrain::new(Direction::Forward, 12);
+    let train = ImmutableLegacyTrainAdaptor { adaptee: legacy_train };
+    let accelerations: [i32; 5] = [5, -5, -5, -12, 5]; //Backward 1
+    let train_driver = |x: ImmutableLegacyTrainAdaptor, y: &i32| x.mov(y);
+    println!("{:?}", accelerations.iter().fold(train, train_driver));
+}
+
+pub fn functional_adapter_use() {
+    println!("-------------------- {} --------------------", file!());
+    let legacy_train = LegacyTrain::new(Direction::Forward, 12);
+    let train = ImmutableLegacyTrainAdaptor { adaptee: legacy_train };
+    println!("{}", to_string(&train));
+    let d1 = Duration::new(5, 0);
+    let d2 = Duration::new(12, 0);
+    //nowy bo to jest inny pociąg z dziedziny "pociągów z możliwymi prędkościami"
+    // po n przekrztałeceniach
+    let new_algebraic_train = train
+        .accelerate(d1).textural_dump()
+        .decelerate(d1).textural_dump()
+        .decelerate(d1).textural_dump()
+        .decelerate(d2).textural_dump()
+        .accelerate(d1).textural_dump();
+    println!("{}", to_string(&new_algebraic_train));
+
+    mutable_collect_to_final_pos();
+    immutable_fold_to_final_pos();
 }
