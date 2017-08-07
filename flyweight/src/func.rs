f27cfebcec405bbfc83b8ba760616c9f956824f6
Artur Augustyniak <artur@aaugustyniak.pl>
2017-08-07 19:35:48 +0200

    properly handled unwrap in flyweight

--- flyweight/src/func.rs
+++ flyweight/src/func.rs
@@ -23,6 +23,7 @@ impl Footer {
 enum FootType {
     Official,
     Private,
+    AddedLater,
 }
 
 #[derive(Debug)]
@@ -37,6 +38,8 @@ pub fn run() {
     println!("-------------------- {} --------------------", file!());
     let a = FootType::Official;
     let b = FootType::Private;
+    let ref c = FootType::AddedLater;
+    let ref stub_footer = Footer { data: "Stub footer".to_string() };
 
     let mut footer_cache = HashMap::new();
     footer_cache.insert(FootType::Official, Footer { data: "Official".to_string() });
@@ -47,17 +50,20 @@ pub fn run() {
         |id| EmployeeData {
             first_name: rand_string(),
             last_name: rand_string(),
-            mail_footer: footer_cache.get(
-                if id % 2 == 0 { &a } else { &b }).unwrap()
+            mail_footer: match footer_cache.get(
+                if id % 2 == 0 { &a } else if id % 9 == 0 { c } else { &b }) {
+                Some(cached_footer) => cached_footer,
+                None => stub_footer
+            }
         }).collect();
 
 
-    for e in employee_names {
+    for e in employee_names.iter() {
         println!("{:?}", e);
         println!("{:p}", e.first_name.as_ptr());
         e.mail_footer.dump_content_address();
     }
 
 
-    //    println!("{:?}", employee_names);
+    println!("{:?}", employee_names);
 }
